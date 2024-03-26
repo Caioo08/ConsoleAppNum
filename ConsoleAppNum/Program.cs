eda9104001af34a22116867fa4d059898a5d87fe
Usuario <Usuario@CPD02-M02>
2024-03-26 10:20:12 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppNum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 1;
+
+            while (cont <= 100)
+            {
+                Console.WriteLine(cont);
+                cont++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
